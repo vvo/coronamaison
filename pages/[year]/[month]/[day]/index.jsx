31c534c06bbbd4afe931d18a574ae335cadc8496
Vincent Voyer <vincent@codeagain.com>
2020-03-25 17:59:57 +0100

    updates

--- pages/[year]/[month]/[day]/index.jsx
+++ pages/[year]/[month]/[day]/index.jsx
@@ -4,8 +4,9 @@ import Head from "next/head";
 import ProgressiveImage from "react-progressive-graceful-image";
 import TwitterIcon from "../../../../svg/twitter.svg?sprite";
 import Line from "../../../../svg/line.svg?sprite";
+import Link from "next/link";
 
-export default function Day({ drawingsForDay, formattedDate, allDates }) {
+export default function Day({ drawingsForDay, date, allDates }) {
   return (
     <div className="container mx-auto px-5">
       <Head>
@@ -18,37 +19,18 @@ export default function Day({ drawingsForDay, formattedDate, allDates }) {
           <a href="/">#coronamaison</a>
         </h1>
 
-        <nav className="col-span-2 hidden sm:block">
-          <h2 className="text-underline text-3xl font-cursive">Jours :</h2>
-          <a
-            className="block text-xl text-blue-800 font-cursive"
-            href="http://www.google.fr"
-          >
-            23/03/2020 <sup>(+20)</sup>
-          </a>
-          <a
-            className="block text-xl text-blue-800 font-cursive"
-            href="http://www.google.fr"
-          >
-            22/03/2020 <sup>(+30)</sup>
-          </a>
-          <a
-            className="block text-xl text-blue-800 font-cursive"
-            href="http://www.google.fr"
-          >
-            21/03/2020 <sup>(+1)</sup>
-          </a>
-        </nav>
+        <Menu allDates={allDates} />
 
         <main className="col-span-12 sm:col-span-10">
           <h2 className="text-underline text-3xl font-cursive text-center">
-            Dessins du 23/03/2020 :
+            {drawingsForDay.length} dessins le {date.day}/{date.month}/
+            {date.year}
           </h2>
 
-          <DrawingsList
-            drawingsForDay={drawingsForDay}
-            formattedDate={formattedDate}
-          />
+          {/* <div className="grid grid-cols-3"> */}
+          <div>
+            <DrawingsList drawingsForDay={drawingsForDay} date={date} />
+          </div>
         </main>
         <footer className="sm:col-start-3 col-span-12 sm:col-span-10"></footer>
       </div>
@@ -56,33 +38,43 @@ export default function Day({ drawingsForDay, formattedDate, allDates }) {
   );
 }
 
-function DrawingsList({ drawingsForDay, formattedDate }) {
-  return (
-    <>
-      {drawingsForDay.map((drawingData) => (
-        <Drawing
-          {...drawingData}
-          formattedDate={formattedDate}
-          key={`${drawingData.source}-${drawingData.id}`}
-        />
-      ))}
-    </>
-  );
+Day.propTypes = {
+  allDates: PropTypes.arrayOf(PropTypes.object),
+  drawingsForDay: PropTypes.arrayOf(PropTypes.object),
+  date: PropTypes.shape({
+    year: PropTypes.string,
+    month: PropTypes.string,
+    day: PropTypes.string,
+  }),
+};
+
+function DrawingsList({ drawingsForDay, date }) {
+  return drawingsForDay.map((drawingData) => (
+    <Drawing
+      {...drawingData}
+      date={date}
+      key={`${drawingData.source}-${drawingData.id}`}
+    />
+  ));
 }
 
 DrawingsList.propTypes = {
   drawingsForDay: PropTypes.arrayOf(PropTypes.object),
-  formattedDate: PropTypes.string,
+  date: PropTypes.shape({
+    year: PropTypes.string,
+    month: PropTypes.string,
+    day: PropTypes.string,
+  }),
 };
 
-function Drawing({ id, source, text, username, formattedDate, image }) {
-  const DrawingImage = require(`../../../../drawings/${formattedDate}/${source}-${id}.jpg?trace`);
+function Drawing({ id, source, username, image }) {
+  const DrawingThumbnail = require(`../../../../public/thumbnails/${source}-${id}.svg?original`);
   const url = `https://twitter.com/${username}/status/${id}`;
 
   return (
-    <>
+    <div>
       <a
-        className="inline-block mt-6 text-lg text-blue-800 hover:text-twitter"
+        className="block mt-6 text-lg text-blue-800 hover:text-twitter"
         href={url}
         target="_blank"
         rel="noopener noreferrer"
@@ -90,11 +82,15 @@ function Drawing({ id, source, text, username, formattedDate, image }) {
         <ProgressiveImage delay={300} placeholder="" src={image}>
           {(src, loading) => {
             if (loading) {
-              return <img src={DrawingImage.trace} />;
+              // return <img style={{ height: "10rem" }} src={image} />;
+              return <img src={DrawingThumbnail} />;
             }
 
             if (src) {
-              return <img src={src} alt="an image" />;
+              return (
+                // <img style={{ height: "10rem" }} src={src} alt="an image" />
+                <img src={src} alt="an image" />
+              );
             }
 
             return (
@@ -105,43 +101,94 @@ function Drawing({ id, source, text, username, formattedDate, image }) {
           }}
         </ProgressiveImage>
         <div className="mt-5">
-          <TwitterIcon className="h-10 inline-block" /> @{username} : « {text} »
+          <TwitterIcon className="h-10 inline-block" /> @{username}
         </div>
       </a>
 
+      {/* <button
+        className="text-3xl"
+        onClick={async () => {
+          await fetch(`/api/drawing/${id}`);
+        }}
+      >
+        delete
+      </button> */}
+
       <Line className="text-yellow-700 h-2 opacity-50 my-6 mx-auto" />
-    </>
+    </div>
   );
 }
 
 Drawing.propTypes = {
   id: PropTypes.string,
   source: PropTypes.string,
-  text: PropTypes.string,
   username: PropTypes.string,
   image: PropTypes.string,
-  formattedDate: PropTypes.string,
+  date: PropTypes.shape({
+    year: PropTypes.string,
+    month: PropTypes.string,
+    day: PropTypes.string,
+  }),
 };
 
-export async function getStaticProps({ params }) {
-  const formattedDate = `${params.year}-${params.month}-${params.day}`;
-  const drawingsForDay = require(`../../../../data/${formattedDate}.json`);
-  // const allDates =
+function Menu({ allDates }) {
+  return (
+    <nav className="col-span-2 hidden sm:block">
+      <h2 className="text-underline text-3xl font-cursive">Jours :</h2>
+      {allDates.map((date) => (
+        <MenuItem date={date} key={JSON.stringify(date)} />
+      ))}
+    </nav>
+  );
+}
+
+Menu.propTypes = {
+  allDates: PropTypes.arrayOf(PropTypes.object),
+};
+
+function MenuItem({ date: { day, month, year, nbDrawings } }) {
+  return (
+    <Link href="/[year]/[month]/[day]" as={`/${year}/${month}/${day}`}>
+      <a className="block text-xl text-blue-800 font-cursive">
+        {day}/{month}/{year} <sup>(+{nbDrawings})</sup>
+      </a>
+    </Link>
+  );
+}
+
+MenuItem.propTypes = {
+  date: PropTypes.shape({
+    day: PropTypes.string,
+    month: PropTypes.string,
+    year: PropTypes.string,
+    nbDrawings: PropTypes.number,
+  }),
+};
+
+export async function getStaticProps({ params: { year, month, day } }) {
+  const date = { year, month, day };
+  const drawingsFolder = `${year}-${month}-${day}`;
+  const drawingsForDay = require(`../../../../data/${drawingsFolder}.json`);
+  const allDates = require("../../../../data/allDates.json");
 
   return {
     props: {
       drawingsForDay,
-      formattedDate,
+      date,
+      allDates,
     },
   };
 }
 
 export async function getStaticPaths() {
+  const allDates = require("../../../../data/allDates.json");
+
   return {
-    paths: [
-      { params: { year: "2020", month: "03", day: "22" } },
-      { params: { year: "2020", month: "03", day: "23" } },
-    ],
+    paths: allDates.map((date) => {
+      return {
+        params: { year: date.year, month: date.month, day: date.day },
+      };
+    }),
     fallback: false,
   };
 }
--- prepareData.js
+++ prepareData.js
@@ -9,46 +9,79 @@ import stream from "stream";
 import { promisify } from "util";
 import mkdirp from "mkdirp";
 import path from "path";
+import sharp from "sharp";
+import sqip from "sqip";
 
 const pipeline = promisify(stream.pipeline);
 
 const adapter = new FileSync("db.json");
 const db = low(adapter);
+const deletes = require("./data/deletes.json").deleted.twitter;
 
 async function run() {
   const drawings = db
     .get("drawings")
-    .slice(0, 30)
+    .filter(({ id_str }) => !deletes.includes(id_str))
     .value();
 
   const drawingsByDate = {};
 
   const worker = async.asyncify(async (drawing) => {
     const date = DateTime.fromJSDate(new Date(drawing.created_at));
-
     const formattedDrawing = {
       source: "twitter",
       id: drawing.id_str,
       username: drawing.user.screen_name,
       text: drawing.text,
+      likes: drawing.retweet_count + drawing.favorite_count,
       image: drawing.extended_entities.media[0].media_url_https,
       date: date.toISO(),
     };
 
+    const extension = path.extname(
+      drawing.extended_entities.media[0].media_url_https,
+    );
+
     const formattedDate = date.toFormat("yyyy-LL-dd");
 
     const drawingsFolder = path.join(__dirname, "drawings", formattedDate);
-    const imagePath = path.join(
-      drawingsFolder,
-      `${formattedDrawing.source}-${formattedDrawing.id}.jpg`,
-    );
+    const thumbnailsFolder = path.join(__dirname, "public/thumbnails");
+    const filename = `${formattedDrawing.source}-${formattedDrawing.id}`;
+    const imagePath = path.join(drawingsFolder, `${filename}.jpg`);
+    const thumbnailPath = path.join(thumbnailsFolder, `${filename}.svg`);
 
     await mkdirp(drawingsFolder);
+    await mkdirp(thumbnailsFolder);
+
     if (!(await fileExists(imagePath))) {
-      await pipeline(
-        got.stream(formattedDrawing.image),
-        fs.createWriteStream(imagePath),
-      );
+      try {
+        const streams = [got.stream(formattedDrawing.image)];
+
+        if (extension !== ".jpg") {
+          streams.push(sharp().toFormat("jpg"));
+        }
+
+        streams.push(fs.createWriteStream(imagePath));
+
+        await pipeline(...streams);
+      } catch (e) {
+        // when there's a 404, image is still created so we delete it and returns
+        if (e.name === "HTTPError") {
+          console.error(formattedDrawing);
+          await fs.promises.unlink(imagePath);
+        }
+
+        return false;
+      }
+    }
+
+    if (!(await fileExists(thumbnailPath))) {
+      await sqip({
+        input: imagePath,
+        output: thumbnailPath,
+        width: 0,
+        plugins: ["sqip-plugin-pixels", "sqip-plugin-svgo"],
+      });
     }
 
     drawingsByDate[formattedDate] = drawingsByDate[formattedDate] || [];
@@ -57,19 +90,30 @@ async function run() {
     return true;
   });
 
-  async.eachLimit(drawings, 2, worker, () => {
+  async.eachLimit(drawings, 4, worker, (err) => {
+    if (err) {
+      throw err;
+    }
+
     const drawingsByDateArray = Object.entries(drawingsByDate);
     const dataFolder = path.join(__dirname, "data");
 
     drawingsByDateArray.forEach(([date, drawings]) => {
       fs.writeFileSync(
         path.join(dataFolder, `${date}.json`),
-        JSON.stringify(drawings, null, 2),
+        JSON.stringify(
+          drawings.sort((a, b) => {
+            return a.likes > b.likes ? -1 : 1;
+          }),
+          null,
+          2,
+        ),
       );
     });
 
-    const allDates = drawingsByDateArray.reduce((acc, [date]) => {
-      acc.push(date);
+    const allDates = drawingsByDateArray.reduce((acc, [date, drawings]) => {
+      const [year, month, day] = date.split("-");
+      acc.push({ year, month, day, nbDrawings: drawings.length });
       return acc;
     }, []);
 
