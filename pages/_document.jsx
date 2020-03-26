e728bd8b4a7bbd9901cf235f42f7c991aaeab630
Vincent Voyer <vincent@codeagain.com>
2020-03-26 18:03:54 +0100

    add simple analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -22,6 +22,14 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
+        <script
+          async
+          defer
+          src="https://cdn.simpleanalytics.io/hello.js"
+        ></script>
+        <noscript>
+          <img src="https://api.simpleanalytics.io/hello.gif" alt="" />
+        </noscript>
       </Html>
     );
   }
