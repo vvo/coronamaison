2cf4d4b4b1def19f450c513fc8d45b7626f095a3
Vincent Voyer <vincent@codeagain.com>
2020-04-06 00:22:42 +0200

    updates

--- findDrawings.js
+++ findDrawings.js
@@ -31,8 +31,8 @@ async function run() {
     const res = await twitter.post("tweets/search/30day/dev.json", {
       json: {
         query: "#coronamaison has:images",
-        fromDate: "202004022159",
-        toDate: "202004042159",
+        fromDate: "202004042159",
+        toDate: "202004052159",
         maxResults: 100,
         next,
       },
